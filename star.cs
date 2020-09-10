a7598119b4d9757941f66c970817a89056c3adfc
JayeshPawar12 <40767196+JayeshPawar12@users.noreply.github.com>
2020-09-10 08:01:39 +0530

    Create star.cs
    Thank you : )

---
+++ star.cs
@@ -0,0 +1,22 @@
+using System;
+ //Print * pyramid 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*"); 
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+
+O/P: *
+     ** 
+     *** 
+     **** 
